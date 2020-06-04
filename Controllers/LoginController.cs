e84120ef062eb8d41ad7930ebf5acf6c08504e73
BanicaM12 <61242987+BanicaM12@users.noreply.github.com>
2020-06-04 14:10:04 +0200

    Add files via upload

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -6,39 +6,98 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BID_E.Models;
+using Microsoft.Data.Sqlite;
+using SQLite.Models;
+using Microsoft.AspNetCore.Http;
+
 
 namespace BID_E.Controllers
 {
+    
     public class LoginController : Controller
     {
-
-        private readonly ILogger<LoginController> _logger;
-
-        public LoginController(ILogger<LoginController> logger)
+        private ApplicationContext db;
+        public LoginController(ApplicationContext _db)
         {
-            _logger = logger;
+            db = _db;
         }
 
-        // GET: /<controller>/
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Login()
+        /*private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }*/
+
+
+       /* public IActionResult Login()
+        {
+            return View();
+        }*/
+
+        
+
+        public ActionResult Register()
         {
             return View();
         }
 
-        public IActionResult Signup()
+        [HttpPost]
+        public ActionResult Register(Login l)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Login.Add(l);
+                db.SaveChanges();
+
+                ModelState.Clear();
+                ViewBag.message = l.Username + " is successfully registered ";
+                return RedirectToAction("Landing", "Graphs");
+            }
+            return View();
+        }
+
+        public ActionResult Login() 
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(Login user)
+        {
+            var account = db.Login.Where(u => u.Username == user.Username && u.Password == user.Password).FirstOrDefault();
+            if(account != null)
+            {
+                HttpContext.Session.SetString("UserID", account.UserId.ToString());
+                HttpContext.Session.SetString("Username", account.Username);
+                //return View("Graphs", "Landing");
+                return RedirectToAction("Landing","Graphs");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Username or Password is incorrect");
+            }
+            return View();
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
         }
     }
 }
