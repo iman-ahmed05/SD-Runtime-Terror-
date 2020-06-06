89b354e59efc24f82ce9eb1869696e28dbc01f2e
aaliyamakda <61203750+aaliyamakda@users.noreply.github.com>
2020-06-06 21:53:45 +0200

    Add files via upload

---
+++ Testing/QueriesControllerTest.cs
@@ -0,0 +1,64 @@
+﻿using BID_E.Controllers;
+using BID_E.Models;
+using SQLite.Models;
+using System;
+using System.Linq;
+using Xunit;
+using Xunit.Sdk;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using Microsoft.Data.Sqlite;
+using System.Configuration;
+
+using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BID_Etests
+{
+    public class QueriesControllerTests
+    {
+        private readonly ILogger<QueriesController> _logger;
+        private DatabaseContext db;
+        QueriesController query;
+        public QueriesControllerTests()
+        {
+            query = new QueriesController(_logger,db);
+        }
+
+        [Fact]
+        public void QueriesControllerTests_genderall()
+        {
+            var viewResult = query.Gender();
+
+            Assert.NotNull(viewResult);
+        }
+    //    [Fact]
+    //    public void QueriesControllerTests_Landing()
+    //    {
+    //        var viewResult = query.Landing();
+
+    //        Assert.NotNull(viewResult);
+    //    }
+    //    [Fact]
+    //    public void QueriesControllerTests_Gen()
+    //    {
+    //        var viewResult = query.Gen();
+
+    //        Assert.NotNull(viewResult);
+    //    }
+    //    [Fact]
+    //    public void QueriesControllerTests_Race()
+    //    {
+    //        var viewResult = query.Race("b");
+
+    //Assert.NotNull(viewResult);
+    //    }
+    }
+}
---
+++ Testing/TestGen.cs
@@ -0,0 +1,276 @@
+﻿using BID_E.Models;
+using Xunit;
+using Xunit.Sdk;
+
+namespace BID_E.Testing
+{
+    public class TestGen
+    {
+        General general = new General
+        {
+            Student_Num = "wkjef",
+            Race = "kfj",
+            Language = "jskdfn",
+            Gender = "jh",
+            Years_In_System = 9,
+            Start_age = 8,
+            End_age = 67,
+            Course_Code = "iefj",
+            Home_Province = "ldjf",
+            Home_Country = "idjfo",
+            AGG_YOS1 = "iwej",
+            PROG_O_YOS1 = "idfji",
+            AGG_YOS2 = "ifjo",
+            PROG_O_YOS2 = "dfh",
+            AGG_YOS3 = "uew",
+            PROG_O_YOS3 = "awd",
+            YOS3_OUT = "sxsz",
+            Start_Year = 458,
+            Reg_End = 388987,
+            Reg_Start = 5478
+
+        };
+        [Fact]
+        public void Student_NumTest()
+        {
+            string expected = "wkjef";
+            Assert.Equal(expected, general.Student_Num);
+
+        }
+        [Fact]
+        public void RaceTest()
+        {
+            string expected = "kfj";
+            Assert.Equal(expected, general.Race);
+
+        }
+        [Fact]
+        public void LanguageTest()
+        {
+            string expected = "jskdfn";
+            Assert.Equal(expected, general.Language);
+
+        }
+        [Fact]
+        public void GenderTest()
+        {
+            string expected = "jh";
+            Assert.Equal(expected, general.Gender);
+
+        }
+        [Fact]
+        public void Years_In_SystemTest()
+        {
+            int expected = 9;
+            Assert.Equal(expected, general.Years_In_System);
+
+        }
+        [Fact]
+        public void Start_ageTest()
+        {
+            int expected = 8;
+            Assert.Equal(expected, general.Start_age);
+
+        }
+        [Fact]
+        public void End_ageTest()
+        {
+            int expected = 67;
+            Assert.Equal(expected, general.End_age);
+
+        }
+        [Fact]
+        public void Course_CodeTest()
+        {
+            string expected = "iefj";
+            Assert.Equal(expected, general.Course_Code);
+
+        }
+        [Fact]
+        public void Home_ProvinceTest()
+        {
+            string expected = "ldjf";
+            Assert.Equal(expected, general.Home_Province);
+
+        }
+        [Fact]
+        public void Home_CountryTest()
+        {
+            string expected = "idjfo";
+            Assert.Equal(expected, general.Home_Country);
+
+        }
+        [Fact]
+        public void AGG_YOS1Test()
+        {
+            string expected = "iwej";
+            Assert.Equal(expected, general.AGG_YOS1);
+
+        }
+        [Fact]
+        public void PROG_O_YOS1Test()
+        {
+            string expected = "idfji";
+            Assert.Equal(expected, general.PROG_O_YOS1);
+
+        }
+        [Fact]
+        public void AGG_YOS2Test()
+        {
+            string expected = "ifjo";
+            Assert.Equal(expected, general.AGG_YOS2);
+
+        }
+        [Fact]
+        public void PROG_O_YOS2Test()
+        {
+            string expected = "dfh";
+            Assert.Equal(expected, general.PROG_O_YOS2);
+
+        }
+        [Fact]
+        public void AGG_YOS3Test()
+        {
+            string expected = "uew";
+            Assert.Equal(expected, general.AGG_YOS3);
+
+        }
+        [Fact]
+        public void PROG_O_YOS3Test()
+        {
+            string expected = "awd";
+            Assert.Equal(expected, general.PROG_O_YOS3);
+
+        }
+        [Fact]
+        public void YOS3_OUTTest()
+        {
+            string expected = "sxsz";
+            Assert.Equal(expected, general.YOS3_OUT);
+
+        }
+        [Fact]
+        public void Start_YearTest()
+        {
+            int expected = 458;
+            Assert.Equal(expected, general.Start_Year);
+
+        }
+        [Fact]
+        public void Reg_EndTest()
+        {
+            int expected = 388987;
+            Assert.Equal(expected, general.Reg_End);
+
+        }
+        [Fact]
+        public void Reg_StartTest()
+        {
+            int expected = 5478;
+            Assert.Equal(expected, general.Reg_Start);
+
+        }
+        LookupOutcome LookUp = new LookupOutcome
+        {
+            outcome = "k",
+            count = 8
+        };
+
+        [Fact]
+        public void LookupOutcomeTest()
+        {
+            string expected = "k";
+            Assert.Equal(expected, LookUp.outcome);
+
+        }
+        [Fact]
+        public void LookupOutcomeCountTest()
+        {
+            int expected = 8;
+            Assert.Equal(expected, LookUp.count);
+
+        }
+        LookupRace Race = new LookupRace
+        {
+            race = "k",
+            count = 8
+        };
+
+        [Fact]
+        public void LookupRaceTest()
+        {
+            string expected = "k";
+            Assert.Equal(expected, Race.race);
+
+        }
+        [Fact]
+        public void LLookupRaceCountTest()
+        {
+            int expected = 8;
+            Assert.Equal(expected, Race.count);
+
+        }
+        LookupAge Age = new LookupAge
+        {
+            End_Age = "k",
+            count = 8
+        };
+
+        [Fact]
+        public void LookupAgeTest()
+        {
+            string expected = "k";
+            Assert.Equal(expected, Age.End_Age);
+
+        }
+        [Fact]
+        public void LookupAgeCountTest()
+        {
+            int expected = 8;
+            Assert.Equal(expected, Age.count);
+
+        }
+        LookupYears Years = new LookupYears
+        {
+            Years_In_System = "k",
+            count = 8
+        };
+
+        [Fact]
+        public void LookupYearsTest()
+        {
+            string expected = "k";
+            Assert.Equal(expected, Years.Years_In_System);
+
+        }
+        [Fact]
+        public void LookupYearsCountTest()
+        {
+            int expected = 8;
+            Assert.Equal(expected, Years.count);
+
+        }
+        LookupProv lookprov = new LookupProv
+        {
+            reg_End = "ijfio",
+            count = 5
+        };
+        [Fact]
+        public void LookupProvTest()
+        {
+            string expected = "ijfio";
+            Assert.Equal(expected, lookprov.reg_End);
+
+        }
+        [Fact]
+        public void LookupProvCountTest()
+        {
+            int expected = 5;
+            Assert.Equal(expected, lookprov.count);
+
+        }
+
+
+
+    }
+}
